4d6cd06801bb982d4c4a19c262fc149eef647a05
Abhishek Raj <raj989881@gmail.com>
2026-01-13 01:24:58 +0530

    initial commit

--- practise/tempCodeRunnerFile.py
+++
@@ -1,9 +0,0 @@
-def check_for_word():
-#       word="Python"  
-#       with open("practise.txt","r") as f:
-#             data=f.read()
-#             if(word in data):
-#                   print("Found")
-#             else:
-#                   print("Not Found")  
-# check_for_word()
